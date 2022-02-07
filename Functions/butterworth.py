d08965a52a014656e9996d565de7e21e6262325b
De Meo <odemeo@usgs.gov>
2022-02-07 12:29:53 -0500

    Created butterworth filter funtion

--- Functions/butterworth.py
+++ Functions/butterworth.py
@@ -1,11 +1,14 @@
-def butterworth(data, samp_rate, order, window)
+from scipy import signal
+
+def butterworth(data, samp_rate, order, window):
     # data = u or v component
     # samp_rate = sampling rate in seconds
     # order = filter order
-    # window = filter window in hours 
+    # window = filter window in hours
     cutOff = 1/(window*3600)
     nyq = (1/samp_rate)/2
     N  = order    
     fc = cutOff / nyq 
     b, a = signal.butter(N, fc)
     filt_data = signal.filtfilt(b, a, data)
+    return filt_data
