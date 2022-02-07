c46394fb3d84a4d6354da4164b492caedb987b03
De Meo <odemeo@usgs.gov>
2022-02-07 12:09:23 -0500

    Created python functions folder

---
+++ Functions/butterworth.py
@@ -0,0 +1,11 @@
+def butterworth(data, samp_rate, order, window)
+    # data = u or v component
+    # samp_rate = sampling rate in seconds
+    # order = filter order
+    # window = filter window in hours 
+    cutOff = 1/(window*3600)
+    nyq = (1/samp_rate)/2
+    N  = order    
+    fc = cutOff / nyq 
+    b, a = signal.butter(N, fc)
+    filt_data = signal.filtfilt(b, a, data)
